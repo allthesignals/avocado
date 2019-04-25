793a304876229a60b7e103a4771ae7e1729086a8
Stuart <stuart.lynn@gmail.com>
2019-04-24 21:00:52 -0400

    implmenting geojson response

--- tiler/routes.js
+++ tiler/routes.js
@@ -5,20 +5,10 @@ import SphericalMercator from '@mapbox/sphericalmercator';
 const mercator = new SphericalMercator({size: 256});
 
 // assumes a standard "geom" column being the geometry
-const performQueryWithGeojson = async (db,query,options) => {
-  const { identifier = 'id' } = options;
-  const { geometry = 'geom' } = options;
-
-  // const queryProperties = await db.any(`
-  //   SELECT
-  //     *
-  //   FROM (
-  //     ${query}
-  //   ) q
-  //   LIMIT 1
-  // `)
-
-  return db.one(`
+const performQueryWithGeojson = async (db, query, options = {}) => {
+  const {identifier = 'id'} = options;
+  const {geometry = 'geom'} = options;
+  const queryStr = `
     SELECT jsonb_build_object(
         'type',     'FeatureCollection',
         'features', jsonb_agg(features.feature)
@@ -31,41 +21,42 @@ const performQueryWithGeojson = async (db,query,options) => {
         'properties', to_jsonb(inputs) - '${identifier}' - '${geometry}'
       ) AS feature
       FROM (${query}) inputs) features;
-  `)
-}
+  `;
+  const result = await db.one(queryStr);
+  console.log('result is ',result)
+  return result 
+};
 
-router.get('/',
-  async ctx => {
-    ctx.body = {key: 'Hey! I make tiles!'};
-  },
-);
+router.get('/', async ctx => {
+  ctx.body = {key: 'Hey! I make tiles!'};
+});
 
-router.get('/query.:ext',
-  async ctx=>{
-    const {req,res,params,query,app} = ctx
-    const {db} = app
-    console.log('query is ',query)
-    const {q,format=''}  = query
-    console.log('trying ', q)
-    switch(format) {
-      case 'geojson': ctx.body = await performQueryWithGeojson(db,q)
-      default: ctx.body = await db.any(q)
-    }
+router.get('/query', async ctx => {
+  const {req, res, params, query, app} = ctx;
+  const {db} = app;
+  console.log('query is ', query.q);
+  const {q, format = ''} = query;
+  console.log('trying ', q);
+  switch (format) {
+    case 'geojson':
+      ctx.body = await performQueryWithGeojson(db, q);
+      break
+    default:
+      ctx.body = await db.any(q);
   }
-)
-router.get('/tiles/:z/:x/:y.mvt',
-  async ctx=>{
-    console.log('processing tile')
-    const {params,query,app} = ctx
-    const {db} = app
-    const {q}  = query
-    const {x,y,z} = params
-    const layer_name = 'layer'
-    console.log('coords ',x,y,z)
-    const bbox = mercator.bbox(x,y,z,false)
-    console.log('bbbox is ',bbox)
+});
+router.get('/tiles/:z/:x/:y.mvt', async ctx => {
+  console.log('processing tile');
+  const {params, query, app} = ctx;
+  const {db} = app;
+  const {q} = query;
+  const {x, y, z} = params;
+  const layer_name = 'layer';
+  console.log('coords ', x, y, z);
+  const bbox = mercator.bbox(x, y, z, false);
+  console.log('bbbox is ', bbox);
 
-    const tile_query= `
+  const tile_query = `
         SELECT ST_AsMVT(q, '${layer_name}', 4096, 'mvt_geom')
     FROM (
       SELECT
@@ -84,11 +75,10 @@ router.get('/tiles/:z/:x/:y.mvt',
       ) c
     ) q
    `;
-    console.log('running query ', q)
+  console.log('running query ', q);
 
-    const result = await db.any(tile_query)
-    ctx.body = result[0].st_asmvt
-  }
-)
+  const result = await db.any(tile_query);
+  ctx.body = result[0].st_asmvt;
+});
 
 export default router;
